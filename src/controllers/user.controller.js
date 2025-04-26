7be14b8446fe077518c1d3c328ef84d17059a54d
DUjjwal <ujjwaldhiman2004@gmail.com>
2025-04-26 20:42:21 +0530

    sub pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -385,3 +385,53 @@ export const getUserChannelProfile = asyncHandler(async (req, res, next) => {
            )
 })
 
+export const getWatchHistory = asyncHandler(async (req, res, next) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            } 
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipline: [
+                                {
+                                    $project: {
+                                        username: 1,
+                                        avatar: 1,
+                                        fullName: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        //data in owner field array of objects need only first element
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+           .status(200)
+           .json(
+            new ApiResponse(200, user[0].wacthHistory, "watch history fetched successfully")
+           )
+})
