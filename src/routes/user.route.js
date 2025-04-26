38c3d20cb9c9fbbbd330c2a2bb2bffd0a58e4196
DUjjwal <ujjwaldhiman2004@gmail.com>
2025-04-26 20:48:46 +0530

    routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js"
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateUserAvatar, updateUsercoverImage } from "../controllers/user.controller.js"
 import { upload } from "../middleware/multer.middleware.js"
 import { verifyJwt } from "../middleware/auth.middleware.js"
 
@@ -24,6 +24,15 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJwt, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJwt, changeCurrentPassword)
+router.route("/current-user").get(verifyJwt, getCurrentUser)
+router.route("/update-account").patch(verifyJwt, changeCurrentPassword)
+router.route("/avatar").post(verifyJwt, upload.single('avatar'),updateUserAvatar)
+router.route("/coverimage").post(verifyJwt, upload.single('coverImage'),updateUsercoverImage)
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile)
+router.route("/history").post(verifyJwt, getWatchHistory)
+
+
 
 
 export default router
